f0e7c36cdcbdce379f919759c08c8e49573f34ad
Jian-Hui Lee <jhlee.dev@gmail.com>
2020-07-01 14:32:15 +0800

    Replace obsolete __sync* API to c11 atomic API

--- src/atomic.h
+++ src/atomic.h
@@ -1,18 +1,24 @@
+#include <stdatomic.h>
 
 struct atomic_t {
     volatile int value;
 };
 
+#define ATOMIC_INIT(v) \
+    {                  \
+        .value = (v)   \
+    }
+
 /* Returns the previously set value */
 static inline int atomic_add(struct atomic_t *a, int i)
 {
-    return __sync_fetch_and_add(&a->value, i);
+    return atomic_fetch_add(&a->value, i);
 }
 
 /* Returns the previously set value */
 static inline int atomic_sub(struct atomic_t *a, int i)
 {
-    return __sync_fetch_and_sub(&a->value, i);
+    return atomic_fetch_sub(&a->value, i);
 }
 
 /* Returns the previously set value */
@@ -30,6 +36,9 @@ static inline int atomic_dec(struct atomic_t *a)
 /* Returns true when the operation was successful. */
 static inline bool atomic_cmpxchg(struct atomic_t *a, int old_i, int new_i)
 {
-    return __sync_bool_compare_and_swap(&a->value, old_i, new_i);
+    /* TODO: is there any better way to do? */
+    struct atomic_t expected, desired;
+    expected.value = old_i;
+    desired.value = new_i;
+    return atomic_compare_exchange_strong(a, &expected, desired);
 }
-
